7e66738707d87f2aabf1aa58e4584bad93eb711e
PBL-48 <94658929PBL-48@users.noreply.github.com>
2026-05-14 00:35:07 +0900

    fix: csvが読み込まれない

--- app.js
+++ app.js
@@ -90,9 +90,6 @@ function init() {
   document.getElementById('shiftsFile').addEventListener('change', onShiftsFile);
   document.getElementById('saveShifts').addEventListener('click', saveShifts);
   document.getElementById('clearShifts').addEventListener('click', clearSavedShifts);
-        placeStalls.forEach(stall=>{
-          stallBlocks.push({ ...stall, shifts: [] });
-        });
   loadSavedShifts();
   loadEmbeddedPlaces();
   loadEmbeddedStalls();
