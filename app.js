b47fbb6a501ec22fa16e5942a7c466c4c95bd53f
PBL-48 <94658929PBL-48@users.noreply.github.com>
2026-05-14 00:27:16 +0900

    fix: 予約者がいませんを削除

--- app.js
+++ app.js
@@ -316,11 +316,6 @@ function showPopupForPlace(place){
         stallBlocks.push({ ...stall, shifts: stallShifts });
       }
     });
-
-    // すべての企画がシフト情報を持たない場合
-    if (stallBlocks.length === 0) {
-      stallBlocks.push({ name: '予約者がいません', owner: '未設定', content: '未設定', shifts: [] });
-    }
   }
 
   const html = stallBlocks.map(stall=>{
