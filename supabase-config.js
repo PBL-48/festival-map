683e48d16d0589f1e97abc9d47936df3fef600c6
PBL-48 <94658929PBL-48@users.noreply.github.com>
2026-07-22 23:30:31 +0900

    fix: supabaseやり直し

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@
 // anon public key は公開されても問題ありません(RLSで保護されているため)。
 // service_role key は絶対にここに書かないでください。
 
-export const SUPABASE_URL = 'https://ugccluznmlfiezpsulbi.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnY2NsdXpubWxmaWV6cHN1bGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2OTM4NTMsImV4cCI6MjEwMDI2OTg1M30.R0qFJ0gFgAgYwMv6PGB5sxCOEg8-uwbXkLmU8EKOI2o';
+export const SUPABASE_URL = 'https://yysxpcoplwqbiheuaqsj.supabase.co/';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5c3hwY29wbHdxYmloZXVhcXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MjQ2MTIsImV4cCI6MjEwMDMwMDYxMn0.cuPNxws8XUWU9kYselrRQTkPWZAnpE97NdbI5fQDcdY';
